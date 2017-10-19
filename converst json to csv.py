685796a64f3140abcb734f5f4ce1d66294dd08b5
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2017-10-18 22:26:18 -0700

    Saving to CSV file

--- converst json to csv.py
+++ converst json to csv.py
@@ -1,7 +1,20 @@
 import json
+from random import shuffle
 
+reviews = []
 with open('reviews.json') as json_data:
-	for line in json_data
-	d = json.load(json_data)
-	print(type(d))
-	print(len(d))
+	for line in json_data:
+		review = json.loads(line)
+		reviews.append(review['reviewText'])
+	
+	
+	print(type(reviews))
+	print(len(reviews))
+	
+shuffle(reviews)
+
+CSV = open("nlp-input.csv", "w")
+
+for i in range(0,300):
+	task_name = "nlp-input-text-" + str(i)
+	CSV.write(task_name + ", \"" + reviews[i] + "\"\n")
