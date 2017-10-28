6787cae23be1355026e1fa3131e9c7828e7c975b
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2017-10-28 14:37:26 -0700

    Stub for coding challenge

---
+++ challenges/linked_list_add.py
@@ -0,0 +1,25 @@
+# Given two numbers, represented as a linked list, where the head is pointing to the
+# most significant digit, write a program that adds the two numbers, and creates a new linked
+# list that stores the sum.
+
+# Create Linked List
+
+# Assign Node.value to first digit
+
+# Create new node
+
+# Assign current Node.next to new node
+
+# Assign new nodes Node.prev to current node
+
+# Assign the new Node.value to second digit
+
+# Create new node
+
+# Assign current Node.next to new node
+
+# Assign new nodes Node.prev to current node
+
+# Assign the new Node.value to first digit + second digit
+
+# Point tail to this last node
