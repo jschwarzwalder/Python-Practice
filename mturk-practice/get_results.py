bfbfa699f191a10865bc9540513874a92e0eab22
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2017-10-28 13:10:52 -0700

    logic to print if there is a result

--- mturk-practice/get_results.py
+++ mturk-practice/get_results.py
@@ -18,3 +18,20 @@ hit_id = 'PASTE_IN_YOUR_HIT_ID'
 # We are only publishing this task to one Worker
 # So we will get back an array with one item if it has been completed
 worker_results = mturk.list_assignments_for_hit(HITId=hit_id, AssignmentStatuses=['Submitted'])
+
+if worker_results['NumResults'] > 0:
+   for assignment in worker_results['Assignments']:
+      xml_doc = xmltodict.parse(assignment['Answer'])
+      
+      print("Worker's answer was:")
+      if type(xml_doc['QuestionFormAnswers']['Answer']) is list:
+         # Multiple fields in HIT layout
+         for answer_field in xml_doc['QuestionFormAnswers']['Answer']:
+            print("For input field: " + answer_field['QuestionIdentifier'])
+            print("Submitted answer: " + answer_field['FreeText'])
+      else:
+         # One field found in HIT layout
+         print("For input field: " + xml_doc['QuestionFormAnswers']['Answer']['QuestionIdentifier'])
+         print("Submitted answer: " + xml_doc['QuestionFormAnswers']['Answer']['FreeText'])
+else:
+   print("No results ready yet")
