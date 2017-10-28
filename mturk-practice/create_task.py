150ffd48656c881d3c2e30f2977d93048b55f593
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2017-10-28 13:04:40 -0700

    changeing from name to file variable

--- mturk-practice/create_task.py
+++ mturk-practice/create_task.py
@@ -10,7 +10,7 @@ mturk = boto3.client('mturk',
 )
 print("I have $" + mturk.get_account_balance()['AvailableBalance'] + " in my Sandbox account")
 
-question = open(name='questions.xml',mode='r').read()
+question = open(file='questions.xml',mode='r').read()
 new_hit = mturk.create_hit(
     Title = 'Is this Tweet happy, angry, excited, scared, annoyed or upset?',
     Description = 'Read this tweet and type out one word to describe the emotion of the person posting it: happy, angry, scared, annoyed or upset',
