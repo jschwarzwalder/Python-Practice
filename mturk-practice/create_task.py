8346c5d297b81d32f541a8684eafa1f5715e6270
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2017-10-28 12:38:20 -0700

    updating for python 3.6

--- mturk-practice/create_task.py
+++ mturk-practice/create_task.py
@@ -4,6 +4,8 @@ mturk = boto3.client('mturk',
    aws_access_key_id = "PASTE_YOUR_IAM_USER_ACCESS_KEY",
    aws_secret_access_key = "PASTE_YOUR_IAM_USER_SECRET_KEY",
    region_name='us-east-1',
+   # In Sandbox, the get_account_balance() call always returns $10,000. 
+   # In order to connect to the live MTurk marketplace, just leave out the endpoint parameter on next line
    endpoint_url = MTURK_SANDBOX
 )
-print "I have $" + mturk.get_account_balance()['AvailableBalance'] + " in my Sandbox account"
+print("I have $" + mturk.get_account_balance()['AvailableBalance'] + " in my Sandbox account")
