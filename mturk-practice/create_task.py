3e08b91baf10b8ea875865692e1fa4a7b6cf3875
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2017-10-28 12:51:56 -0700

    Defining a HIT

--- mturk-practice/create_task.py
+++ mturk-practice/create_task.py
@@ -9,3 +9,21 @@ mturk = boto3.client('mturk',
    endpoint_url = MTURK_SANDBOX
 )
 print("I have $" + mturk.get_account_balance()['AvailableBalance'] + " in my Sandbox account")
+
+question = open(name='questions.xml',mode='r').read()
+new_hit = mturk.create_hit(
+    Title = 'Is this Tweet happy, angry, excited, scared, annoyed or upset?',
+    Description = 'Read this tweet and type out one word to describe the emotion of the person posting it: happy, angry, scared, annoyed or upset',
+    Keywords = 'text, quick, labeling',
+    Reward = '0.15',
+    MaxAssignments = 1,
+    LifetimeInSeconds = 172800,
+    AssignmentDurationInSeconds = 600,
+    AutoApprovalDelayInSeconds = 14400,
+    Question = question,
+)
+print "A new HIT has been created. You can preview it here:"
+print "https://workersandbox.mturk.com/mturk/preview?groupId=" + new_hit['HIT']['HITGroupId']
+# Remember to modify the URL above when you're publishing
+# HITs to the live marketplace.
+# Use: https://worker.mturk.com/mturk/preview?groupId=
