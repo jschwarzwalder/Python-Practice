74919fbe90c2efe2fd31daa9e8d294671f4b4e84
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2017-10-30 00:19:54 -0700

    new solution for join int arrays

--- challenges/join_array_int.py
+++ challenges/join_array_int.py
@@ -5,40 +5,14 @@
 # 65,535 is max 16 bit integer
 
 def function (int_array):
-	ones = []
-	tens = []
-	hundres = []
-	thousands = []
-	ten_thousands = []
-	for int in int_array:
-		if int > 9999:
-			
-			if len(ten_thousands) == 0:
-				ten_thousands.append(int)
-			else:
-				# insert in order to ten_thousands
-		elif int > 999: 
-			if len(thousands) == 0:
-				thousands.append(int)
-			else:
-				# insert in order to thousands
+	new_int_array = sorted(int_array, cmp = compare)
+	print(new_int_array)
 	
-	new_int_array = []
-	ones_index
-	tens_index
-	hundreds_index
-	thous_index
-	ten_thous_index
-	# compare arrays and add largest left most integer next
-	new_int = one[ones_index]
-	ones_index += 1
-	if new_int < tens[tens_index] % 10:
-		new_int = tens[tens_index]
-		tens_index += 1
-		ones_index -= 1
+	
+def compare (a, b):
+	AB = int(str(a) + str(b))
+	BA = int(str(b) + str(a))
+	return AB - BA
 		
-			
-	for index in range(0,len(new_int_array)):
-		end_num
-	
-	
+		
+function([2, 20, 4, 100])
