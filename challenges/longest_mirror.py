4fc728a160f43fcbb24d0d368c455b8c64f6619f
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2017-10-30 00:19:29 -0700

    new challenge

---
+++ challenges/longest_mirror.py
@@ -0,0 +1,10 @@
+# https://www.youtube.com/watch?v=TLaGwTnd3HY
+
+# Given a string, find the largest mirrored string. doob (is a mirrored string).
+# AFHIMOTUVWXY - mirror looks the same
+# BCDEGJKLNPQRSZ - doesn’t have a mirrored equivalent.
+# ilovwx - mirror looks the same
+# acdefghjkmnrstuyz - doesn’t have a mirrored equivalent.
+# p<>q - these mirror each other
+# b<>d - these mirror each other
+
